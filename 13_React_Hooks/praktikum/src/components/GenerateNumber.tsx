027c3e19972d0d972996c97d665603d0768d2ac1
Naufal Nasrullah <naufaln176@gmail.com>
2023-09-18 07:13:45 +0700

    task_13: done!

---
+++ 13_React_Hooks/praktikum/src/components/GenerateNumber.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+const GenerateNumberBtn = () => {
+  const [number, setNumber] = useState(0);
+
+  const generateNumber = () => {
+    const randomNumber = Math.round(Math.random() * 100);
+    setNumber(randomNumber);
+    console.log(randomNumber);
+  };
+
+  return (
+    <section style={{ marginTop: "220px" }}>
+      <div className="d-flex flex-column justify-content-center align-items-center mt-4 mb-5">
+        <button
+          style={{ width: "fit-content", padding: "5px 15px" }}
+          onClick={() => {
+            generateNumber();
+          }}
+        >
+          Tebak Angkamu!
+        </button>
+        <h5 className="mt-2 text-center px-3">
+          {number || "Mainkan game tebak angka 1 - 100 dengan klik tombol di atas"}
+        </h5>
+      </div>
+    </section>
+  );
+};
+
+export default GenerateNumberBtn;
